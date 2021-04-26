5368b29085cc8b3ab12c634c1f08ff013fa7a3d1
dovimaj <dovimajowka@gmail.com>
2021-04-26 17:04:38 +0300

    Update musician-form
    * use bootstrap
    * input validation

--- pages/musician-form.js
+++ pages/musician-form.js
@@ -1,13 +1,48 @@
 import React from "react";
+import { Container, Button, Form, FormGroup, Label, Input } from "reactstrap";
+import styles from "../styles/Form.module.scss";
 
 const musician_form = () => {
   return (
-    <div>
-      <form>
-        <label></label>
-        <input></input>
-      </form>
-    </div>
+    <Container>
+      <Form className={styles.form}>
+        <h1>Musician Registration</h1>
+        <FormGroup>
+          <Label htmlFor="name">Name:</Label>
+          <Input type="text" required id="name" name="name"></Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label htmlFor="instrument">Instrument:</Label>
+          <Input
+            type="text"
+            required
+            id="instrument"
+            name="instrument"
+          ></Input>{" "}
+        </FormGroup>
+
+        <FormGroup>
+          <Label htmlFor="date-of-birth">Date of birth:</Label>
+          <Input
+            max={new Date().toISOString().split("T")[0]}
+            type="date"
+            required
+            id="date-of-birth"
+            name="date-of-birth"
+          ></Input>
+        </FormGroup>
+
+        <FormGroup>
+          <Label htmlFor="image">Profile Image:</Label>
+          <Input type="file" required></Input>
+        </FormGroup>
+
+        <Button style={{ width: "100%" }} color="primary">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
