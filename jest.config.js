568b9511c11b27e6f3f5c6f01dd792910978f2b1
dovimaj <dovimajowka@gmail.com>
2021-04-30 05:10:03 +0300

    Mock files and css modules with Jest config

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,8 @@ module.exports = {
     "^@components(.*)$": "<rootDir>/components$1",
     "^@pages(.*)$": "<rootDir>/pages$1",
     "^@hooks(.*)$": "<rootDir>/hooks$1",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|scss|less)$": "identity-obj-proxy",
   },
 };
